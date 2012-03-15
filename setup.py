80d96081b6cb1b10545cf8e51219a2b73f598240
Marc Brinkmann <git@marcbrinkmann.de>
2012-03-15 14:21:39 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding=utf8
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='slugger',
+      version='0.1dev',
+      description="""Slugging done right. Tries to support close to 300
+                     languages.""",
+      long_description=read('README.rst'),
+      keywords='slug slugging web i18n',
+      author='Marc Brinkmann',
+      author_email='git@marcbrinkmann.de',
+      url='http://github.com/mbr/slugger',
+      license='LGPL',
+      install_requires=['remember', 'logbook', 'unihandecode'],
+      include_package_data=True,
+      packages=find_packages(exclude=['glibcparse']),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+          'Operating System :: OS Independent',
+          'Topic :: Internet',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
+          'Topic :: Software Development :: Internationalization',
+     ]
+     )
