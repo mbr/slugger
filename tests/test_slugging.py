47a344041e02dc04480ab4cb60138eeb37ed12d2
Marc Brinkmann <git@marcbrinkmann.de>
2015-05-05 18:51:18 +0200

    Fixed some tests.

--- tests/test_slugging.py
+++ tests/test_slugging.py
@@ -7,6 +7,6 @@ from slugger import Slugger
 def test_simple_slug():
     s = Slugger('de', hanlang='ja')
 
-    assert s.sluggify(u'Hellö & Wörld 漢字') == u'helloe-und-woerld-kan-ji'
+    assert s.sluggify(u'Hellö & Wörld 漢字') == u'helloe-und-woerld-kanji'
     assert s.sluggify(u'And they lived happily ever after!') == \
-        u'and-they-lived-happily-ever-after-'
+        u'and-they-lived-happily-ever-after'
